45fb4c450bac2f822fff55fafcf9fe53c25885a6
Suresh Ramappa <suresh_ramappa@condenast.com>
2021-04-06 18:05:32 +0530

    User profile using the existing HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import UserList from './components/user-list/UserList';
+import UserList from "./components/user-list/UserList";
+import UserProfile from "./components/user-profile/UserProfile";
 
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <UserList datasource="https://jsonplaceholder.typicode.com/users" />
+      <UserProfile
+        name="Suresha Ram"
+        email="suriram.sam@gmail.com"
+        datasource="https://jsonplaceholder.typicode.com/posts"
+      />
     </div>
   );
 }
